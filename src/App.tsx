e89c3fa573257670b0b1d01fd6ae49633c9c051a
warrior25 <eetu.hakkinen@hotmail.com>
2026-02-26 16:49:10 +0200

    Test update

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
 
   return (
     <main>
-      <h1>My todos</h1>
+      <h1>My todos are awesome</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
